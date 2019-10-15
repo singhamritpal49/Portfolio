87b9f57212873a432c14a0215a5aaad6bd776b74
Your Name <you@example.com>
2019-10-15 12:06:15 -0400

    Updates

---
+++ precache-manifest.7fbcf1aac47f26a9a3d0ba65e6a2b31d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e760277b1ff83e0a010aa69bf1d2c1",
+    "url": "amritpalsingh.dev/index.html"
+  },
+  {
+    "revision": "6391949e06a38f4da446",
+    "url": "amritpalsingh.dev/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "ed874c9f818f36cd2225",
+    "url": "amritpalsingh.dev/static/css/main.92010362.chunk.css"
+  },
+  {
+    "revision": "6391949e06a38f4da446",
+    "url": "amritpalsingh.dev/static/js/2.79d4b334.chunk.js"
+  },
+  {
+    "revision": "ed874c9f818f36cd2225",
+    "url": "amritpalsingh.dev/static/js/main.b640d3f2.chunk.js"
+  },
+  {
+    "revision": "927d549356bf8b8e10e0",
+    "url": "amritpalsingh.dev/static/js/runtime-main.ef9f925c.js"
+  },
+  {
+    "revision": "c7d1bb94b19b5723df3f8345013fb6f9",
+    "url": "amritpalsingh.dev/static/media/Enlight117.c7d1bb94.jpg"
+  },
+  {
+    "revision": "b6e99d117dc89aab29f50f267c75a797",
+    "url": "amritpalsingh.dev/static/media/profilepic.b6e99d11.png"
+  }
+]);
