ca45cc6aa59ea12ea21cc9cd756aa21b24dff21d
Your Name <you@example.com>
2019-10-15 18:36:09 -0400

    Updates

---
+++ precache-manifest.dc74eb2028d2723d03ce82889fe3a153.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a24dce303d3e300ea1a99024e643a364",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "096c3e0a4140241750e1",
+    "url": "/portfolio/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "89c1d8b4af9b8725d323",
+    "url": "/portfolio/static/css/main.070b1eec.chunk.css"
+  },
+  {
+    "revision": "096c3e0a4140241750e1",
+    "url": "/portfolio/static/js/2.8e455c91.chunk.js"
+  },
+  {
+    "revision": "89c1d8b4af9b8725d323",
+    "url": "/portfolio/static/js/main.52ddc554.chunk.js"
+  },
+  {
+    "revision": "791f997dc0acbb20fb93",
+    "url": "/portfolio/static/js/runtime-main.021c856b.js"
+  },
+  {
+    "revision": "c7d1bb94b19b5723df3f8345013fb6f9",
+    "url": "/portfolio/static/media/Enlight117.c7d1bb94.jpg"
+  },
+  {
+    "revision": "b6e99d117dc89aab29f50f267c75a797",
+    "url": "/portfolio/static/media/profilepic.b6e99d11.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.d525b6c8543754bd05761266f072f5e2.js"
+  "/portfolio/precache-manifest.dc74eb2028d2723d03ce82889fe3a153.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
